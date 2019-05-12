943d402d0af04d8f5b235f8c6b93d79dda552caf
DS-Minetest <vorunbekannt75@web.de>
2019-05-12 20:36:44 +0200

    rename stuff to avoid crashes

--- init.lua
+++ init.lua
@@ -30,18 +30,18 @@ minetest.register_craftitem("matrix_test:item", {
 		end
 		minetest.chat_send_all("spawning facedir_to_matrix test particle")
 		local node = minetest.get_node(pointed_thing.under)
-		local facedir_matrix = minetest.facedir_to_matrix(node.param2, true)
+		local facedir_matrix = minetest.facedir_to_matrix3(node.param2, true)
 		local facedir_matrix
 		if minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].paramtype2 == "wallmounted" then
-			facedir_matrix = minetest.wallmounted_to_matrix(node.param2, true)
+			facedir_matrix = minetest.wallmounted_to_matrix3(node.param2, true)
 		else
-			facedir_matrix = minetest.facedir_to_matrix(node.param2, true)
+			facedir_matrix = minetest.facedir_to_matrix3(node.param2, true)
 		end
 		--~ minetest.chat_send_all(dump_matrix(facedir_matrix))
 		local relative_pos = {x=0.2, y=0.4, z=-0.6}
-		minetest.chat_send_all(minetest.pos_to_string(matrix.multiply(facedir_matrix, relative_pos)))
+		minetest.chat_send_all(minetest.pos_to_string(matrix3.multiply(facedir_matrix, relative_pos)))
 		minetest.add_particle({
-			pos = vector.add(pointed_thing.under, matrix.multiply(facedir_matrix, relative_pos)),
+			pos = vector.add(pointed_thing.under, matrix3.multiply(facedir_matrix, relative_pos)),
 			velocity = {x=0, y=0, z=0},
 			acceleration = {x=0, y=0, z=0},
 			expirationtime = 5,
@@ -73,21 +73,21 @@ minetest.register_entity("matrix_test:ent", {
 		--~ minetest.chat_send_all("bla")
 		--~ minetest.chat_send_all(minetest.pos_to_string(rot))
 		--~ rot = vector.add(rot, vector.multiply(vector.new(1, 1, 0), dtime))
-		local oldm = matrix.from_yaw_pitch_roll(rot)
-		local rotchange = matrix.rotation_around_vector(vector.new(1,0,0), 0.1)
-		local mat = matrix.multiply(rotchange, oldm)
-		--~ local mat = matrix.multiply(matrix.identity, oldm)
-		local nrot = matrix.to_yaw_pitch_roll(mat)
+		local oldm = matrix3.from_pitch_yaw_roll(rot)
+		local rotchange = matrix3.rotation_around_vector(vector.new(1,0,0), 0.1)
+		local mat = matrix3.multiply(rotchange, oldm)
+		--~ local mat = matrix3.multiply(matrix3.identity, oldm)
+		local nrot = matrix3.to_pitch_yaw_roll(mat)
 		--~ nrot = vector.add(nrot, vector.multiply(vector.new(0, 1, 0), dtime))
 		minetest.chat_send_all(minetest.pos_to_string(vector.divide(nrot, math.pi)))
 		self.object:set_rotation(nrot)
-		--~ local mat = matrix.from_yaw_pitch_roll(rot)
-		--~ mat = matrix.round(mat)
+		--~ local mat = matrix3.from_pitch_yaw_roll(rot)
+		--~ mat = matrix3.round(mat)
 
 		-- add particles that rotate around the object
 		local relative_pos = {x=0.2, y=0.4, z=-0.6}
 		minetest.add_particle({
-			pos = vector.add(self.object:get_pos(), matrix.multiply(mat, relative_pos)),
+			pos = vector.add(self.object:get_pos(), matrix3.multiply(mat, relative_pos)),
 			velocity = {x=0, y=0, z=0},
 			acceleration = {x=0, y=0, z=0},
 			expirationtime = dtime*1,
@@ -97,7 +97,7 @@ minetest.register_entity("matrix_test:ent", {
 		})
 		relative_pos.x = -2
 		minetest.add_particle({
-			pos = vector.add(self.object:get_pos(), matrix.multiply(mat, relative_pos)),
+			pos = vector.add(self.object:get_pos(), matrix3.multiply(mat, relative_pos)),
 			velocity = {x=0, y=0, z=0},
 			acceleration = {x=0, y=0, z=0},
 			expirationtime = dtime*1,
@@ -121,7 +121,7 @@ minetest.register_chatcommand("nent", {
 
 local playerbubble = {
 	pos = {x=2, y=0, z=0},
-	rotmat = matrix.new(matrix.identity),
+	rotmat = matrix3.new(matrix3.identity),
 }
 
 minetest.register_globalstep(function(dtime)
@@ -131,7 +131,7 @@ minetest.register_globalstep(function(dtime)
 	end
 	--~ minetest.chat_send_all(dump_matrix(playerbubble.rotmat))
 	--~ minetest.chat_send_all(dump(playerbubble.pos))
-	playerbubble.pos = matrix.multiply(playerbubble.rotmat, playerbubble.pos)
+	playerbubble.pos = matrix3.multiply(playerbubble.rotmat, playerbubble.pos)
 	local playerpos = get_player_head_pos(player)
 	minetest.add_particle({
 		pos = vector.add(playerpos, playerbubble.pos),
@@ -153,7 +153,7 @@ minetest.register_chatcommand("pb", {
 		if not a then
 			return false, "no_vec"
 		end
-		playerbubble.rotmat = matrix.rotation_around_vector(a, 0.1)
+		playerbubble.rotmat = matrix3.rotation_around_vector(a, 0.1)
 		return true, "ok"
 	end,
 })
